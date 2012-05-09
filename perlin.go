987986e0e5392831f1ac5a58a7f6a1c1a6174c1c
Ian Davis <me@iandavis.com>
2012-05-09 13:57:46 +0100

    Revised function to expose alpha and beta parameters

--- perlin.go
+++ perlin.go
@@ -12,29 +12,21 @@ import (
 )	
 
 
-var primes = [5][3]int64 {
-							[3]int64{15731, 789221, 1376312589},
-							[3]int64{99877, 103867, 9576890767},
-							[3]int64{27653, 222437, 2860486313},
-							[3]int64{30181, 326083, 3367900313},
-							[3]int64{52709, 450299, 5915587277},
-
-						}
 
 
-func noise(x, y int64, seed int64, octave int) float64 {
+func noise(x, y int64, seed int64) float64 {
 	n := x + y*57 + seed*7
 	fn := (n << 13) ^ n
 	return (1.0 - float64((fn*(fn*fn*15731+789221)+1376312589)&0x7fffffff)/float64(0x40000000))
 }
 
-func smoothedNoise(x float64, y float64, seed int64, octave int) float64 {
+func smoothedNoise(x float64, y float64, seed int64) float64 {
 	xint := int64(math.Trunc(x))
 	yint := int64(math.Trunc(y))
 
-	corners := (noise(xint-1, yint-1, seed, octave) + noise(xint+1, yint-1, seed, octave) + noise(xint-1, yint+1, seed, octave) + noise(xint+1, yint+1, seed, octave)) / 16
-	sides := (noise(xint-1, yint, seed, octave) + noise(xint+1, yint, seed, octave) + noise(xint, yint-1, seed, octave) + noise(xint, yint+1, seed, octave)) / 8
-	center := noise(xint, yint, seed, octave) / 4
+	corners := (noise(xint-1, yint-1, seed) + noise(xint+1, yint-1, seed) + noise(xint-1, yint+1, seed) + noise(xint+1, yint+1, seed)) / 16
+	sides := (noise(xint-1, yint, seed) + noise(xint+1, yint, seed) + noise(xint, yint-1, seed) + noise(xint, yint+1, seed)) / 8
+	center := noise(xint, yint, seed) / 4
 	return corners + sides + center
 }
 
@@ -44,7 +36,7 @@ func interpolate(a, b, x float64) float64 {
 	return a*(1-f) + b*f
 }
 
-func interpolatedNoise(x, y float64, seed int64, octave int) float64 {
+func interpolatedNoise(x, y float64, seed int64) float64 {
 
 	xint := math.Trunc(x)
 	xfrac := x - xint
@@ -52,10 +44,10 @@ func interpolatedNoise(x, y float64, seed int64, octave int) float64 {
 	yint := math.Trunc(y)
 	yfrac := y - yint
 
-	v1 := smoothedNoise(xint, yint, seed, octave)
-	v2 := smoothedNoise(xint+1, yint, seed, octave)
-	v3 := smoothedNoise(xint, yint+1, seed, octave)
-	v4 := smoothedNoise(xint+1, yint+1, seed, octave)
+	v1 := smoothedNoise(xint, yint, seed)
+	v2 := smoothedNoise(xint+1, yint, seed)
+	v3 := smoothedNoise(xint, yint+1, seed)
+	v4 := smoothedNoise(xint+1, yint+1, seed)
 
 	i1 := interpolate(v1, v2, xfrac)
 	i2 := interpolate(v3, v4, xfrac)
@@ -64,12 +56,17 @@ func interpolatedNoise(x, y float64, seed int64, octave int) float64 {
 
 }
 
-func Noise2D(x, y float64, seed int64, persistence float64, octaves int) (value float64) {
+func Noise2D(x, y float64, seed int64, alpha float64, beta float64, octaves int) (value float64) {
 
+
+	scale := 1.0
 	for i := 0; i < octaves; i++ {
-		frequency := math.Pow(2, float64(i))
-		amplitude := math.Pow(persistence, float64(i))
-		value += interpolatedNoise(x*frequency, y*frequency, seed, i) * amplitude
+		value += interpolatedNoise(x, y, seed) / scale
+
+		scale *= alpha
+		x *= beta
+		y *= beta
 	}
+
 	return
 }
